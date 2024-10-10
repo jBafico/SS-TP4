10994031eddda940577d5c198d3ed238c10f032e
Juan Cruz Bafico <jbafico@itba.edu.ar>
2024-10-10 10:48:38 -0300

    feat: ej2_b graph

--- Visualization/grapher_ex2_.py
+++ Visualization/grapher_ex2_.py
@@ -31,6 +31,32 @@ def amplitude_vs_omega_graph(amplitude_vals, omega_vals):
 
     print(f"Graphic saved to '{file_path}'")
 
+
+def amplitude_vs_omega_graph_with_k(amplitude_vals, omega_vals, k_vals):
+
+    directory= 'Ej2_b'
+
+    ensure_output_directory_creation(directory)
+    
+    for k in k_vals:
+        # Graficar ω vs amplitud para cada valor de k
+        plt.plot(omega_vals, amplitude_vals, label=f'k = {k:.0f} N/m')
+
+    plt.xlabel('ω (rad/s)')
+    plt.ylabel('Amplitud (m)')
+    plt.grid(True)
+    
+    # Define the output file path with dt in the filename
+    file_path = os.path.join(directory, f"omega_vs_amplitude_per_k.png")
+
+    # Save the plot to the file
+    plt.savefig(file_path)
+
+    # Optionally, you can clear the current figure to prevent overlay issues in future plots
+    plt.clf()
+
+    print(f"Graphic saved to '{file_path}'")
+
 def ensure_output_directory_creation(directory):
     # Check if the directory exists, if not, create it
     if not os.path.exists(directory):
