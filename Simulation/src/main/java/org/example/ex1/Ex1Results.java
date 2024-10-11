e5f5ca4a5664f9b3a55a3a01351db18faf576e0d
sranucci <sranucci@itba.edu.ar>
2024-10-11 12:17:12 -0300

    JVM memory usage reduced significantly,

--- Simulation/src/main/java/org/example/ex1/Ex1Results.java
+++ Simulation/src/main/java/org/example/ex1/Ex1Results.java
@@ -10,11 +10,3 @@ public record Ex1Results (
         Ex1Params params,
         Map<Integer, List<ResultsForDt>> resultsByRepetitionNo
 ) implements Results {}
-
-record ResultsForDt(
-        double dt,
-        List<Ex1Particle> analytical,
-        List<Ex1Particle> beeman,
-        List<Ex1Particle> gear5,
-        List<Ex1Particle> verlet
-) {}
---
+++ Simulation/src/main/java/org/example/ex1/ResultsForDt.java
@@ -0,0 +1,21 @@
+package org.example.ex1;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ResultsForDt {
+
+    public double dt;
+    public List<Ex1Particle> analytical;
+    public List<Ex1Particle> beeman;
+    public List<Ex1Particle> gear5;
+    public List<Ex1Particle> verlet;
+
+}
